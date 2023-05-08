843baee1fe2b187c9b8239e0c194509ba00b367f
Alonso Portillo <92688817+Porti45@users.noreply.github.com>
2023-05-08 18:07:01 +0200

    Add files via upload

---
+++ aissprueba/src/main/java/prueba/aissprueba/AisspruebaApplication.java
@@ -0,0 +1,13 @@
+package prueba.aissprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisspruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisspruebaApplication.class, args);
+	}
+
+}
---
+++ aissprueba/src/test/java/prueba/aissprueba/AisspruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.aissprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisspruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
